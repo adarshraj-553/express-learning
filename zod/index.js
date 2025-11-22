13f427d6bd6761b4ece15d74ce808bbaa577e003
adarshraj-553 <adarshkeshrirx2.7@gmail.com>
2025-11-22 19:02:39 +0530

    zod

---
+++ zod/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const zod = require('zod')
+const app = express()
+
+const schema = zod.array(zod.string())
+
+app.use(express.json())
+
+app.post('/home', (req,res) => {
+    const data = req.body.text;
+    const response = schema.safeParse(data)
+    res.send({
+        response
+    })
+    
+    console.log(response.success)
+    
+})
+
+app.listen(1000)
